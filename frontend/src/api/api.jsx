208db8c8cecd2818883fa81191f7131fb37bbc91
Ritwik <gritwik5843@gmail.com>
2026-01-13 05:27:23 +0530

    frontend +backend

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3123/api',
+  baseURL: 'https://company-ticket-raising-1.onrender.com/api',
     withCredentials: true
 });
 
