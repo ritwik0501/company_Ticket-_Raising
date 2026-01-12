ff066d98131790b8af9aff0a0257b268b17f9818
Ritwik <gritwik5843@gmail.com>
2026-01-13 05:06:18 +0530

    update link

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3123/api',
+  baseURL: 'https://company-ticket-raising-m4ff.vercel.app/api',
     withCredentials: true
 });
 
